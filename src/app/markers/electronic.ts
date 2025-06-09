0c3c229758e5942b7e2c9f27bf24f544b592fdb4
Azylium <sleedjeuxvideo@gmail.com>
2025-06-09 16:19:51 +0200

    add electronic component data

---
+++ src/app/markers/electronic.ts
@@ -0,0 +1,41 @@
+export const Electronic: { lat: number; lng: number; name: string }[] = [
+  {
+    lat: -32.69486597787506, lng: -109.95117187500001, name: 'Composant électronique',
+  }, {
+    lat: -83.50029457577654, lng: -49.48242187500001, name: 'Composant électronique'
+  }, {
+    lat: -78.37600402593652, lng: -51.767578125, name: 'Composant électronique'
+  },
+  {
+    lat: -78.98785907036147, lng: -4.262523800045882, name: 'Composant électronique'
+  },
+  {
+    lat: -72.19524581118353, lng: 19.423828125000004, name: 'Composant électronique'
+  },
+  {
+    lat: -75.8128929869713,lng: 6.8115234375, name: 'Composant électronique'
+  },
+  {
+    lat: -40.58058466412763,lng: -35.59570312500001, name: 'Composant électronique'
+  },
+  {
+    lat: -62.08331486294794,lng: -49.35058593750001, name: 'Composant électronique'
+  },
+  {
+    lat: -28.9600886880068,lng: -26.630859375, name: 'Composant électronique'
+  },
+  {
+    lat: -17.644022027872726,lng: 62.35839843750001, name: 'Composant électronique'
+  },
+  {
+    lat: 32.953368145799345,lng: -5.888671875, name: 'Composant électronique'
+  },
+  {
+    lat: 59.489726035537075,lng: 34.58496093750001, name: 'Composant électronique'
+  },
+  {
+    lat: 77.87881372624746, lng: -10.678710937500002, name: 'Composant électronique'
+  }
+  ]
+
+
