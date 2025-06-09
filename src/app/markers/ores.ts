c3743d8cc6e9bae20235fbec67eabfd850d2b78c
Azylium <sleedjeuxvideo@gmail.com>
2025-06-09 16:19:58 +0200

    add ores data with geographical coordinates

---
+++ src/app/markers/ores.ts
@@ -0,0 +1,17 @@
+export const Ores: { lat: number; lng: number; name: string }[] = [
+  {
+    lat: 16.088042220148818,lng: 69.56542968750001, name: 'Minerais'
+  },
+  {
+    lat: -65.98227002980873, lng: 62.66601562500001, name: 'Minerais'
+  },
+  {
+    lat: -37.718590325588146, lng: 70.83984375000001, name: 'Minerais'
+  },
+  {
+    lat: 21.37124437061832, lng: 71.85058593750001, name: 'Minerais'
+  },
+  {
+    lat: 14.944784875088372, lng: 70.53222656250001, name: 'Minerais'
+  }
+]
