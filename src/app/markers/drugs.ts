422ee9ba6d131d9fa42dc7286aa7a4e3ef27229f
Azylium <sleedjeuxvideo@gmail.com>
2025-06-12 19:57:55 +0200

    Add drugs data with locations and names

---
+++ src/app/markers/drugs.ts
@@ -0,0 +1,84 @@
+export const Drugs: { lat: number; lng: number; name: string }[] = [
+  {
+    lat
+      :
+      -56.340901204199085,
+    lng
+      :
+      12.875976562500002,
+    name: '13 Mirror Park Boulevard',
+  },
+  {
+    lat
+      :
+      -54.31652324025827,
+    lng
+      :
+      8.349609375000002,
+    name: '985 Mirror Park Boulevard',
+  },
+  {
+    lat
+      :
+      -57.20771009775018,
+    lng
+      :
+      12.436523437500002,
+    name: '2 Bridge Street',
+  },
+  {
+    lat
+      :
+      -60.19615576604439,
+    lng
+      :
+      19.907226562500004,
+    name: '98 Mirror Park Boulevard',
+  },
+  {
+    lat
+      :
+      -55.65279803318956,
+    lng
+      :
+      0.26367187500000006,
+    name: '3197 Elgin Avenue',
+  },
+  {
+    lat
+      :
+      -59.57885104663186,
+    lng
+      :
+      -33.35449218750001,
+    name: '378 Palomino Avenue',
+  },
+  {
+    lat
+      :
+      -57.421294392094055,
+    lng
+      :
+      -13.139648437500002,
+    name: '129 Alta Street'
+  },
+  {
+    lat
+      :
+      -65.05360170595502,
+    lng
+      :
+      -31.508789062500004,
+    name: '321 Vespucci Boulevard'
+  },
+  {
+    lat
+      :
+      -54.85131525968608,
+    lng
+      :
+      -15.732421875000002,
+    name: '985 Hawick Avenue'
+  },
+  ]
+
